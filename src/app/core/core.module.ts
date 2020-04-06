53769fa28de6b4dfabc6fa89082262be2880fc65
ViNo G <vinovinothg@gmail.com>
2020-04-06 12:46:39 +0530

    Add sructure of the header component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { HeaderComponent } from "./components/header/header.component";
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { UserAvatarComponent } from "./components/user-avatar/user-avatar.component";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
-  declarations: [HeaderComponent, SidebarComponent],
+  declarations: [HeaderComponent, SidebarComponent, UserAvatarComponent],
   exports: [HeaderComponent, SidebarComponent],
-  imports: [CommonModule]
+  imports: [CommonModule, RouterModule],
 })
 export class CoreModule {}
